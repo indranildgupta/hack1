70efdf713f50b94ab882cdfdb614a9277768669e
prateekpilaniwala <67147349+prateekpilaniwala@users.noreply.github.com>
2021-10-30 19:31:47 +0530

    Java Program to find the longest common subsequence in a given string.
    Please accept my solution for HacktoberFest2021

---
+++ Longest_Common_Subsequence.java
@@ -0,0 +1,50 @@
+import java.io.*;
+public class LongestCommonSubsequence
+{
+  public int lcs( char[] X, char[] Y, int m, int n )
+  {
+    int L[][] = new int[m+1][n+1];
+
+    /* Following steps build L[m+1][n+1] in bottom up fashion. Note
+      that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
+    for (int i=0; i<=m; i++)
+    {
+      for (int j=0; j<=n; j++)
+      {
+        if (i == 0 || j == 0)
+          L[i][j] = 0;
+        else if (X[i-1] == Y[j-1])
+          L[i][j] = L[i-1][j-1] + 1;
+        else
+          L[i][j] = max(L[i-1][j], L[i][j-1]);
+      }
+    }
+  return L[m][n];
+  }
+
+  /* Function to get maximum of 2 integers */
+  public int max(int a, int b)
+  {
+    return (a > b)? a : b;
+    //using ternary operator
+  }
+
+  // main method
+  public static void main(String[] args)
+  {
+    // creating object
+    LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+    Scanner sc=new Scanner(System.in);
+    String s1=sc.nextLine();
+    String s2=sc.nextLine();
+
+    char[] X=s1.toCharArray();
+    char[] Y=s2.toCharArray();
+
+    int m = X.length;
+    int n = Y.length;
+
+    System.out.println("Length of LCS is" + " " + lcs.lcs( X, Y, m, n ) );
+  }
+}
+
